8091665c948b5d12c23fc7d946bccb9cbbd346d7
Quek Yang Sheng Tyson <quektyson@gmail.com>
2016-11-04 00:37:33 +0800

    Add /getAuthors route

--- server/js/gitfunctions.js
+++ server/js/gitfunctions.js
@@ -3,8 +3,7 @@ const HTTPS = 'https://';
 const REPOS_DIR = 'repositories/';
 let repoPath = REPOS_DIR + '';
 
-const spawn = require( 'child_process' )
-    .spawn;
+const CHILD_PROCESS = require('child_process');
 // const shortlog = spawn('git', ['shortlog', '-n'], { stdio: ['inherit'] });
 //
 // ls.stdout.on('data', (data) => {
@@ -22,54 +21,140 @@ const spawn = require( 'child_process' )
 
 
 
-var cloneRepo = ( repoUrl ) => {
-    const clone = spawn( 'git', [ 'clone', repoUrl, REPOS_DIR + getRepoName( repoUrl ) ], {
-        stdio: [ 'inherit' ]
-    } );
+var gitClone = (repoUrl, callback) => {
+  console.log('git clone');
+  let repoPath = REPOS_DIR + getRepoName(repoUrl);
+  // start executing
+  let command = CHILD_PROCESS.exec(`git clone ${repoUrl} ${repoPath}`, function (error, stdout, stderr) {
+    if (error) {
+      console.log(error.stack);
+      console.log('Error code: ' + error.code);
+      console.log('Signal received: ' + error.signal);
+      callback(error, stdout, repoPath);
+      return;
+    }
+    // success case
+    callback(error, stdout, repoPath);
+  });
 
-    clone.stdout.on( 'data', ( data ) => {
-        console.log( `stdout: ${data}` );
-        console.log( typeof data );
-    } );
-
-    clone.stderr.on( 'data', ( data ) => {
-        console.log( `stderr: ${data}` );
-    } );
-
-    clone.on( 'close', ( code ) => {
-        console.log( `child process exited with code ${code}` );
-    } );
+  command.on('exit', function (code) {
+    console.log('Child process exited with exit code ' + code);
+  });
 
 }
 
-exports.gitBlame = ( repoUrl, callback ) => {
-    var childProcess = require( 'child_process' ),
-        gitblame;
+//git log --format='%aN' | sort -u
+//get all authors
+//for each author, do the next command to get json obj
 
-    gitblame = childProcess.exec( `cd ${REPOS_DIR+getRepoName(repoUrl)} && git blame -L:"Slot" scrapy/core/engine.py`, function( error, stdout, stderr ) {
-        if ( error ) {
-            console.log( error.stack );
-            console.log( 'Error code: ' + error.code );
-            console.log( 'Signal received: ' + error.signal );
-        }
-        // console.log( 'Child Process STDOUT: ' + stdout );
-        //console.log( 'Child Process STDERR: ' + stderr );
-        callback( stdout );
-    } );
+//git log --shortstat --author="Bevin" | grep -E "fil(e|es) changed" | awk '{inserted+=$4; deleted+=$6} END {print "additions: ", inserted, "deletions: ", deleted }'
+//With a given author name, return me a json obj in this format {name: "name1", additions:"40", deletions:"30"}
+//insert into data.members array
 
-    gitblame.on( 'exit', function( code ) {
-        console.log( 'Child process exited with exit code ' + code );
-    } );
+// var data = {
+//   "members": [
+//     {name: "name1", additions:"40", deletions:"30"},
+//     {name: "name2", additions:"30", deletions:"20"},
+//     {name: "name3", additions:"20", deletions:"30"},
+//     {name: "name4", additions:"30", deletions:"60"},
+//     {name: "name5", additions:"40", deletions:"40"},
+//     {name: "name6", additions:"10", deletions:"20"},
+//     {name: "name7", additions:"30", deletions:"40"}
+//   ]
+// };
+
+
+
+exports.getAuthors = (repoUrl, callback) => {
+  console.log('getAuthors');
+  let arguments = "--format='%aN' | sort -u";
+  gitClone(repoUrl, (error, data, repoPath) => {
+    if (isRepoExist(data)) {
+      gitLog(repoPath, arguments, (error, data) => {
+        let json = stringToJsonArray(data);
+        callback(error, json);
+      });
+    } else { // assume clone is successful for now
+      gitLog(repoPath, arguments, (error, data) => {
+        let json = stringToJsonArray(data);
+        callback(error, json);
+      });
+    }
+
+  });
+}
+
+/*only accept \n delimited string*/
+var stringToJsonArray = (string) => {
+  let arrayOfData = string.split('\n');
+  let json = '[';
+  for (var i = 0; i < arrayOfData.length; i++) {
+
+    //if every stdout produce a trailing empty line, then this equality check would have to stay
+    if (arrayOfData[i].length === 0) {
+      break;
+    }
+
+    if (i) { //only append when it is not the first. hacky yet genius. credits to SO
+      json += (',');
+    }
+
+    console.log(arrayOfData[i]);
+    json += `{${arrayOfData[i]}}`;
+  }
+  json += ']';
+  return json;
+}
+
+var gitLog = (repoPath, arguments, callback) => {
+  console.log('git log');
+  // start executing
+  let command = CHILD_PROCESS.exec(`cd ${repoPath} && git log ${arguments}`, function (error, stdout, stderr) {
+    if (error) {
+      console.log(error.stack);
+      console.log('Error code: ' + error.code);
+      console.log('Signal received: ' + error.signal);
+      callback(error, stdout);
+      return;
+    }
+    // success case
+    callback(error, stdout);
+  });
+
+  command.on('exit', function (code) {
+    console.log('Child process exited with exit code ' + code);
+  });
+
+}
+
+exports.gitBlame = (repoUrl, callback) => {
+  var childProcess = require('child_process'),
+    gitblame;
+
+  gitblame = childProcess.exec(`cd ${REPOS_DIR+getRepoName(repoUrl)} && git blame -L:"Slot" scrapy/core/engine.py`, function (error, stdout, stderr) {
+    if (error) {
+      console.log(error.stack);
+      console.log('Error code: ' + error.code);
+      console.log('Signal received: ' + error.signal);
+    }
+    // console.log( 'Child Process STDOUT: ' + stdout );
+    //console.log( 'Child Process STDERR: ' + stderr );
+    callback(stdout);
+  });
+
+  gitblame.on('exit', function (code) {
+    console.log('Child process exited with exit code ' + code);
+  });
 }
 
 
 
 var showGitLog = () => {
-    exec( `cd repositories/github.com/vuejs/awesome-vue.git && git shortlog`, function( err, stdout, stderr ) {
-        console.log( err );
-        console.log( stdout );
-        console.log( stderr );
-    } );
+  exec(`cd repositories/github.com/vuejs/awesome-vue.git && git shortlog`, function (err, stdout, stderr) {
+    console.log(err);
+    console.log(stdout);
+    console.log(stderr);
+  });
 }
 
 var showGitBlame = () => {
@@ -77,27 +162,29 @@ var showGitBlame = () => {
 
 }
 
-var getRepoName = ( repoUrl ) => {
-    let repoName = repoUrl;
+var getRepoName = (repoUrl) => {
+  let repoName = repoUrl;
 
-    if ( repoName.startsWith( HTTPS, 0 ) ) {
-        repoName = repoName.replace( HTTPS, '' );
-    }
+  if (repoName.startsWith(HTTPS, 0)) {
+    repoName = repoName.replace(HTTPS, '');
+  }
 
-    if ( repoName.startsWith( HTTP, 0 ) ) {
-        repoName = repoName.replace( HTTP, '' );
-    }
+  if (repoName.startsWith(HTTP, 0)) {
+    repoName = repoName.replace(HTTP, '');
+  }
 
-    console.log( `repoUrl: ${repoUrl}` );
-    console.log( `RepoName: ${repoName}` );
+  console.log(`repoUrl: ${repoUrl}`);
+  console.log(`RepoName: ${repoName}`);
 
-    return repoName;
+  return repoName;
 }
 
+// TODO: create a few more helper functions to determine download failure
+
 /*this method is a hack. may fail if the library api changes its error message*/
-var isRepoExist = ( reasonForFailure ) => {
-    let errorMessage = reasonForFailure.toString();
-    return errorMessage.includes( 'exists and is not an empty directory' );
+var isRepoExist = (reasonForFailure) => {
+  let errorMessage = reasonForFailure.toString();
+  return errorMessage.includes('exists and is not an empty directory');
 }
 
 // this.gitBlame( 'https://github.com/scrapy/scrapy.git' );
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,14 +1,15 @@
-var express = require( 'express' );
-var gitfunctions = require( '../js/gitfunctions' );
+var express = require('express');
+var gitfunctions = require('../js/gitfunctions');
 var router = express.Router();
 
 /* GET home page. */
-router.get( '/gitblame', function( req, res, next ) {
-    gitfunctions.gitBlame( 'https://github.com/scrapy/scrapy.git', ( result ) => {
-        res.status( 200 )
-            .send( result );
-    } );
+router.get('/getAuthors', function (req, res, next) {
+  gitfunctions.getAuthors('https://github.com/scrapy/scrapy.git', (error, data) => {
 
-} );
+    res.status(200)
+      .send(data);
+  });
+
+});
 
 module.exports = router;
