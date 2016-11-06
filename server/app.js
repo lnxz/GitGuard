3f7d6d057e6c35fdbc30a42f98199584175dbc84
Quek Yang Sheng Tyson <quektyson@gmail.com>
2016-11-06 09:37:49 +0000

    shift app.use(cors())) to the top of the configurations

--- server/app.js
+++ server/app.js
@@ -20,6 +20,7 @@ app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(cors());
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
@@ -27,7 +28,6 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(cors());
 
 app.use('/', routes);
 // app.use('/users', users);
