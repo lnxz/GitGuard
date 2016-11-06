ecc9de32f65317e046625aa3b00028f2a0c87585
Quek Yang Sheng Tyson <quektyson@gmail.com>
2016-11-06 06:55:49 +0000

    Update routes to handle query string. e.g. /authors?repo=some_url /commits?repo=someurl&author=some_author

--- server/routes/index.js
+++ server/routes/index.js
@@ -4,31 +4,55 @@ var router = express.Router();
 
 /* GET all authors */
 router.get('/authors', function (req, res, next) {
-  gitfunctions.getAuthors('https://github.com/scrapy/scrapy.git', (error, data) => {
+  let repoUrl = req.query.repo
 
+  // this equality check is not safe
+  if (repoUrl != null) {
+    gitfunctions.getAuthors(repoUrl, (error, data) => {
+      res.status(200)
+        .send(data);
+    });
+
+  } else {
     res.status(200)
-      .send(data);
-  });
-
+      .send('{}');
+  }
 });
 
 /* GET all authors addition deletion */
 router.get('/authorsAdditionsDeletions', function (req, res, next) {
-  gitfunctions.getAuthorsAdditionsDeletions('https://github.com/scrapy/scrapy.git', (error, data) => {
+  let repoUrl = req.query.repo
 
+  // this equality check is not safe
+  if (repoUrl != null) {
+    gitfunctions.getAuthorsAdditionsDeletions(repoUrl, (error, data) => {
+      res.status(200)
+        .send(data);
+    });
+
+  } else {
     res.status(200)
-      .send(data);
-  });
+      .send('{}');
+  }
+
 
 });
 
 /* GET all authors stability */
 router.get('/authorsStability', function (req, res, next) {
-  gitfunctions.getAuthorsStability('https://github.com/scrapy/scrapy.git', (error, data) => {
+  let repoUrl = req.query.repo
 
+  // this equality check is not safe
+  if (repoUrl != null) {
+    gitfunctions.getAuthorsStability(repoUrl, (error, data) => {
+      res.status(200)
+        .send(data);
+    });
+
+  } else {
     res.status(200)
-      .send(data);
-  });
+      .send('{}');
+  }
 
 });
 
@@ -38,16 +62,16 @@ router.get('/commits', function (req, res, next) {
   let authorName = req.query.author
 
   // this equality check is not safe
-  if(repoUrl != null && authorName != null){
+  if (repoUrl != null && authorName != null) {
     console.log(repoUrl)
     console.log(authorName)
     gitfunctions.getAuthorsCommits(repoUrl, authorName, (error, data) => {
       res.status(200)
         .send(data);
     });
-  } else{
+  } else {
     res.status(200)
-      .send('');
+      .send('{}');
   }
 
 
